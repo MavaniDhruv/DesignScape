068d40f5564287cc1663e6345097e88b08e43b98
chintan <chintanrajodiya8098@gmail.com>
2024-03-13 20:01:47 +0530

    four line code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './Components/Header';
+import Home1 from './Components/Home1';
 
 function App() {
   return (
    <>
    <Header></Header>
+   <Home1></Home1>
    </>
   );
 }
---
+++ src/Components/Home1.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+function Home1() {
+  return (
+    <>
+        <Container>
+            <div className='home1-main'></div>
+        </Container>
+    </>
+  )
+}
+
+export default Home1
